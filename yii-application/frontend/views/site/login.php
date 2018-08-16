60d82fc248d783c8aefa79cef0365a411743b759
fangqi <fangqi@yinglaijinrong.com>
2018-08-16 17:52:06 +0800

    21312

--- yii-application/frontend/views/site/login.php
+++ yii-application/frontend/views/site/login.php
@@ -1,39 +1,97 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>秀百贝后台登录系统</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+    <link rel="stylesheet" href="./css/font.css">
+    <link rel="stylesheet" href="./css/xadmin.css">
+    <link rel="stylesheet" href="https://cdn.bootcss.com/Swiper/3.4.2/css/swiper.min.css">
+    <script type="text/javascript" src="https://cdn.bootcss.com/jquery/3.2.1/jquery.min.js"></script>
+    <script type="text/javascript" src="https://cdn.bootcss.com/Swiper/3.4.2/js/swiper.jquery.min.js"></script>
+    <script src="./lib/layui/layui.js" charset="utf-8"></script>
+    <script type="text/javascript" src="./js/x-admin.js"></script>
+    <style>
 
-/* @var $this yii\web\View */
-/* @var $form yii\bootstrap\ActiveForm */
-/* @var $model \common\models\LoginForm */
+        .layui-form-pane .layui-form-label {
+            padding: 10px 27px;
+        }
+    </style>
+</head>
+<body>
+<div class="login-logo"><h1>秀百贝后台登录系统</h1></div>
+<div class="login-box">
+    <form class="layui-form layui-form-pane" action="index.php?r=site/login" id="login" method="post">
 
-use yii\helpers\Html;
-use yii\bootstrap\ActiveForm;
-
-$this->title = 'Login';
-$this->params['breadcrumbs'][] = $this->title;
-?>
-<div class="site-login">
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>Please fill out the following fields to login:</p>
-
-    <div class="row">
-        <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
-
-                <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
-
-                <?= $form->field($model, 'password')->passwordInput() ?>
-
-                <?= $form->field($model, 'rememberMe')->checkbox() ?>
-
-                <div style="color:#999;margin:1em 0">
-                    If you forgot your password you can <?= Html::a('reset it', ['site/request-password-reset']) ?>.
-                </div>
-
-                <div class="form-group">
-                    <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
-                </div>
-
-            <?php ActiveForm::end(); ?>
+        <h3>登录你的帐号</h3>
+        <label class="login-title" for="username">帐号</label>
+        <div class="layui-form-item">
+            <label class="layui-form-label login-form"><i class="iconfont">&#xe6b8;</i></label>
+            <div class="layui-input-inline login-inline">
+                <input type="text" name="user_name" lay-verify="required" placeholder="请输入你的帐号" autocomplete="off" class="layui-input">
+            </div>
+        </div>
+        <label class="login-title" for="password">密码</label>
+        <div class="layui-form-item">
+            <label class="layui-form-label login-form"><i class="iconfont">&#xe82b;</i></label>
+            <div class="layui-input-inline login-inline">
+                <input type="password" name="password" lay-verify="required" placeholder="请输入你的密码" autocomplete="off" class="layui-input">
+            </div>
+        </div>
+        <div class="form-actions">
+            <button class="btn btn-warning pull-right" lay-submit lay-filter="login"  type="submit">登录</button>
+            <div class="forgot"><a href="#" class="forgot">忘记帐号或者密码</a></div>
+        </div>
+    </form>
+</div>
+<div class="bg-changer">
+    <div class="swiper-container changer-list">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide"><img class="item" src="./images/a.jpg" alt=""></div>
+            <div class="swiper-slide"><img class="item" src="./images/b.jpg" alt=""></div>
+            <div class="swiper-slide"><img class="item" src="./images/c.jpg" alt=""></div>
+            <div class="swiper-slide"><img class="item" src="./images/d.jpg" alt=""></div>
+            <div class="swiper-slide"><img class="item" src="./images/e.jpg" alt=""></div>
+            <div class="swiper-slide"><img class="item" src="./images/f.jpg" alt=""></div>
+            <div class="swiper-slide"><img class="item" src="./images/g.jpg" alt=""></div>
+            <div class="swiper-slide"><img class="item" src="./images/h.jpg" alt=""></div>
+            <div class="swiper-slide"><img class="item" src="./images/i.jpg" alt=""></div>
+            <div class="swiper-slide"><img class="item" src="./images/j.jpg" alt=""></div>
+            <div class="swiper-slide"><img class="item" src="./images/k.jpg" alt=""></div>
+            <div class="swiper-slide"><span class="reset">初始化</span></div>
         </div>
     </div>
+    <div class="bg-out"></div>
+    <div id="changer-set"><i class="iconfont">&#xe696;</i></div>
 </div>
+<script src="/layui/jquery.js"></script>
+<script src="/layui/ajaxfrom.js"></script>
+<script>
+    $(function  () {
+        layui.use('form', function(){
+            var form = layui.form;
+            $('#login').ajaxForm(function(data) {
+                if(data.code == 500){
+                    layer.alert(data.msg);
+                }else{
+                    window.location.href = 'index.php';
+                }
+
+            });
+        });
+    })
+
+</script>
+<script>
+    //百度统计可去掉
+    var _hmt = _hmt || [];
+    (function() {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?b393d153aeb26b46e9431fabaf0f6190";
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(hm, s);
+    })();
+</script>
+</body>
+</html
